98cdc373666ec80433562e7c3f16a5c475737826
llinjae <dldlswognqh@naver.com>
2023-05-15 15:18:05 +0900

    content 및 edit button navigation

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -1,11 +1,23 @@
+import { useNavigate } from 'react-router-dom';
 import MyButton from './MyButton';
 
 const DiaryItem = ({ id, emotion, content, date }) => {
+  const navigate = useNavigate();
+
   const strDate = new Date(parseInt(date)).toLocaleDateString();
+
+  const goDetail = () => {
+    navigate(`/diary/${id}`);
+  };
+
+  const goEdit = () => {
+    navigate(`/edit/${id}`);
+  };
 
   return (
     <div className='DiaryItem'>
       <div
+        onClick={goDetail}
         className={[
           'emotion_img_wrapper',
           `emotion_img_wrapper_${emotion}`,
@@ -13,12 +25,12 @@ const DiaryItem = ({ id, emotion, content, date }) => {
       >
         <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} />
       </div>
-      <div className='info_wrapper'>
+      <div onClick={goDetail} className='info_wrapper'>
         <div className='diary_date'>{strDate}</div>
         <div className='diary_content_preview'>{content.slice(0, 25)}</div>
       </div>
       <div className='btn_wrapper'>
-        <MyButton text={'수정하기'} />
+        <MyButton onClick={goEdit} text={'수정하기'} />
       </div>
     </div>
   );
