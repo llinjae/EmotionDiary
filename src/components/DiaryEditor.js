07ec28778272a6f7b892ef75460aa8fbfd0f22d4
llinjae <dldlswognqh@naver.com>
2023-05-15 20:18:18 +0900

    새 일기 쓰기 취소/완료 button 기능 구현

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
-import { useRef, useState } from 'react';
+import { useContext, useRef, useState } from 'react';
+import { DiaryDispatchContext } from './../App.js';
 
 import MyHeader from './MyHeader';
 import MyButton from './MyButton';
@@ -43,11 +44,22 @@ const DiaryEditor = () => {
   const [emotion, setEmotion] = useState(3);
   const [date, setDate] = useState(getStringDate(new Date()));
 
+  const { onCreate } = useContext(DiaryDispatchContext);
   const handleClickEmote = (emotion) => {
     setEmotion(emotion);
   };
 
   const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    if (content.length < 1) {
+      contentRef.current.focus();
+      return;
+    }
+
+    onCreate(date, content, emotion);
+    navigate('/', { replace: true });
+  };
 
   return (
     <div className='DiaryEditor'>
@@ -90,6 +102,16 @@ const DiaryEditor = () => {
               ref={contentRef}
               value={content}
               onChange={(e) => setContent(e.target.value)}
+            />
+          </div>
+        </section>
+        <section>
+          <div className='control_box'>
+            <MyButton text={'취소하기'} onClick={() => navigate(-1)} />
+            <MyButton
+              text={'작성완료'}
+              type={'positive'}
+              onClick={handleSubmit}
             />
           </div>
         </section>
