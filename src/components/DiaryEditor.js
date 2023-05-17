b83a4051fe00fd7cab1b2d00e1111b12b1066a0a
llinjae <dldlswognqh@naver.com>
2023-05-17 14:22:51 +0900

    일기 내용 쓸 때 리렌더링 방지 최적화

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { useContext, useEffect, useRef, useState } from 'react';
+import { useCallback, useContext, useEffect, useRef, useState } from 'react';
 import { DiaryDispatchContext } from './../App.js';
 import { getStringDate } from '../util/date.js';
 import { emotionList } from '../util/emotion.js';
@@ -15,9 +15,9 @@ const DiaryEditor = ({ isEdit, originData }) => {
   const [date, setDate] = useState(getStringDate(new Date()));
 
   const { onCreate, onEdit, onRemove } = useContext(DiaryDispatchContext);
-  const handleClickEmote = (emotion) => {
+  const handleClickEmote = useCallback((emotion) => {
     setEmotion(emotion);
-  };
+  }, []);
 
   const navigate = useNavigate();
 
